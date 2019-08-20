907c39e8d1d271973c7907fd4e14c33546039120
Porrith Suong <psuong95@gmail.com>
2019-08-20 15:21:58 -0400

    Changes the ReactiveDisposalSystem to declare base types in the class definition.

--- Scripts/Systems/Unmanaged/ReactiveDisposalSystem.cs
+++ Scripts/Systems/Unmanaged/ReactiveDisposalSystem.cs
@@ -3,18 +3,26 @@ using Unity.Entities;
 using Unity.Jobs;
 using System;
 
+#if ENABLE_VALIDATION
+using static UnityEngine.Debug;
+#endif
+
 namespace Reactive.Unmanaged.Systems {
 
     [UpdateInGroup(typeof(DisposalGroup))]
-    public abstract class ReactiveDisposalSystem : JobComponentSystem {
+    public abstract class ReactiveDisposalSystem<T> : JobComponentSystem where T : struct, ISystemStateComponentData,
+           IDisposable {
 
         [ExcludeComponent(typeof(UnmanagedMemTag))]
-        public struct DisposalJob<T> : IJobForEachWithEntity<T> where T : struct, ISystemStateComponentData, IDisposable {
+        public struct DisposalJob : IJobForEachWithEntity<T> {
 
             [ReadOnly]
             public EntityCommandBuffer CmdBuffer;
 
             public void Execute(Entity entity, int index, ref T c0) {
+#if ENABLE_VALIDATION
+                Log($"Disposing unmanaged memory of Type: {c0.GetType()} from Entity: {entity.Index}");
+#endif
                 c0.Dispose();
                 CmdBuffer.RemoveComponent(entity, typeof(T));
             }
@@ -26,8 +34,12 @@ namespace Reactive.Unmanaged.Systems {
             cmdBufferSystem = World.Active.GetOrCreateSystem<BeginPresentationEntityCommandBufferSystem>();
         }
 
-        protected JobHandle ScheduleDisposalJob<T>(JobHandle inputDeps) where T : struct, ISystemStateComponentData, IDisposable {
-            var job = new DisposalJob<T> {
+        protected override void OnDestroy() {
+            ScheduleDisposalJob(default).Complete();
+        }
+
+        protected JobHandle ScheduleDisposalJob(JobHandle inputDeps) {
+            var job = new DisposalJob {
                 CmdBuffer = cmdBufferSystem.CreateCommandBuffer()
             }.Schedule(this);
 
--- Scripts/Systems/UpdateGroup/DisposalSystemGroup.cs
+++ Scripts/Systems/UpdateGroup/DisposalSystemGroup.cs
@@ -12,6 +12,6 @@ namespace Reactive {
     /// The system group that will actually run and schedule the destruction of whatever entity and dispose any kind of 
     /// unmanaged memory.
     /// </summary>
-    [UpdateInGroup(typeof(PresentationSystemGroup)), UpdateAfter(typeof(DisposalGroup))]
+    [UpdateInGroup(typeof(PresentationSystemGroup)), UpdateAfter(typeof(DisposalTriggerGroup))]
     public class DisposalGroup : ComponentSystemGroup { }
 }
